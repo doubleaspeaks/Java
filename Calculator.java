6d46ac13b0c868b453186d90b452688b4a004d58
doubleaspeaks <81939607+doubleaspeaks@users.noreply.github.com>
2021-09-21 17:21:43 -0400

    Update and rename Calculator_runner.java to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,14 +6,14 @@ class Main {
     char operator;
     Double number1, number2, result;
 
-    // create an object of Scanner class
+    
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
+    
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
+    
     System.out.println("Enter first number");
     number1 = input.nextDouble();
 
@@ -22,25 +22,25 @@ class Main {
 
     switch (operator) {
 
-      // performs addition between numbers
+      //  addition  
       case '+':
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
         break;
 
-      // performs subtraction between numbers
+      //  subtraction  
       case '-':
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
 
-      // performs multiplication between numbers
+      //  multiplication  
       case '*':
         result = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + result);
         break;
 
-      // performs division between numbers
+      //  division  
       case '/':
         result = number1 / number2;
         System.out.println(number1 + " / " + number2 + " = " + result);
